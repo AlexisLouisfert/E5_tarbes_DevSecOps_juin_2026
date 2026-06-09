16e4cc94c93823aa4c98de1f5881309f404e1d35
AlexisLouisfert <72492809+AlexisLouisfert@users.noreply.github.com>
2026-06-09 10:49:54 +0200

    Create ApplicationTest.java

---
+++ src/test/java/com/demo/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // Ce test vérifie simplement que l'application réussit à démarrer
+        // Sans erreur, l'étape 04 du Jenkinsfile sera un succès !
+    }
+}
